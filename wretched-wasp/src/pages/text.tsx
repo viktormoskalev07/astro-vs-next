5d54d5a4ca3da35d162748799b0d2c1633b1d9a8
vmoskalev@smart-ui.pro <viktormoskalev07@gmail.com>
2025-02-18 18:50:55 +0200

    start

---
+++ wretched-wasp/src/pages/text.tsx
@@ -0,0 +1,24 @@
+import {Nav} from "../layouts/nav.tsx";
+
+const getRandomHexColor = () => {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0")}`;
+};
+
+
+
+
+export const Test = ()=>{
+
+    const test=Array(100).fill("")
+    return <section>
+        <Nav/>
+        <div style={{display: "grid", gridTemplateColumns: "repeat( 10, 1fr) "}}>
+            {test.map((item, key) => {
+                const color = getRandomHexColor()
+                return <div style={{background: color}} key={key}>
+                    hello +{key}
+                </div>
+            })}
+        </div>
+    </section>
+}
